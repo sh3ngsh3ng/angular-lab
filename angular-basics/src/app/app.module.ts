f2c41a31c4f5657cd97c67beb7cb480ebede2afd
sh3ngsh3ng <yshenglim13@gmail.com>
2022-03-16 10:12:53 +0000

    added extra for exercise 2

--- angular-basics/src/app/app.module.ts
+++ angular-basics/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
